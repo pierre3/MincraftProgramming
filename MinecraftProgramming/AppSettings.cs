a4330ff023629b0f1d09794d025efe845de5ca16
Hirotada Kobayashi <pierre3@outlook.com>
2022-12-11 11:18:43 +0900

    typo projectname

---
+++ MinecraftProgramming/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿internal class AppSettings
+{
+    public MinecraftServer MinecraftServer { get; set; } = new MinecraftServer();
+}
+
+class MinecraftServer
+{
+    public string Address { get; set; } = "127.0.0.1";
+    public string Port { get; set; } = "25575";
+    public string Pass { get; set; } = "password";
+}
---
+++ MinecraftProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var builder = ConsoleApp.CreateBuilder(args);
+builder.ConfigureServices((ctx, services) =>
+{
+    services.Configure<AppSettings>(ctx.Configuration);
+});
+var app = builder.Build();
+app.AddCommands<MinecraftApp>();
+app.Run();
